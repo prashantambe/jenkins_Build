2c88141f2fbee259717f66bf4bde1d6c61d5dfe8
Prashant Ambe <prahant101@gmail.com>
2022-05-03 11:23:16 +0530

    change 100 to50

--- jenkins_MY.java
+++ jenkins_MY.java
@@ -1,6 +1,6 @@
 class JavaExample {
    public static void main(String args[]) {
-	int n = 100;
+	int n = 50;
 	System.out.print("Numbers from 1 to "+n+" are: ");
 	for (int i = 1; i <= n; i++) {
 		System.out.print(i + " ");
